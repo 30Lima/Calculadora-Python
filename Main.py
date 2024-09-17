1854b27da005c0777fda49883f4d05154ef80e5e
Pedro Lima <pedrolimaa354@gmail.com>
2024-09-16 23:21:09 -0300

    WIP: Menu de opções adicionado.

---
+++ Main.py
@@ -0,0 +1,15 @@
+#Calculadora em python
+
+import os
+
+os.system("cls")
+
+op_usuario = int(input("------MENU-----\n"
+                         "1 - Adição\n"
+                         "2 - Subtração\n"
+                         "3 - Multiplicação\n"
+                         "4 - Divisão\n"
+                         "--------------------\n"
+                         "Digite o número da opção desejada: "))
+
+
