d03af7883f8eeb8439bfd1a62481f6b00c31a08b
Pedro Lima <pedrolimaa354@gmail.com>
2024-09-17 23:39:33 -0300

    WIP: Função soma adicionada.

--- Main.py
+++ Main.py
@@ -4,12 +4,17 @@ import os
 
 os.system("cls")
 
+#Acrescentando as funções com parâmetro
+
+def somar (numero1,numero2):
+    return numero1+numero2
+
 op_usuario = int(input("------MENU-----\n"
                          "1 - Adição\n"
                          "2 - Subtração\n"
                          "3 - Multiplicação\n"
                          "4 - Divisão\n"
                          "--------------------\n"
-                         "Digite o número da opção desejada: "))
+                        "Digite o número da opção desejada: "))
 
 
