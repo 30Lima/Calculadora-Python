f191f3d37982ac68d21177bd8e005b5f87b54ea1
Pedro Lima <pedrolimaa354@gmail.com>
2024-09-20 16:31:31 -0300

    WIP: Refatorização do menu de opções.

--- Main.py
+++ Main.py
@@ -15,12 +15,21 @@ def multiplicar (numero1,numero2): #Função para a multiplicação de dois núm
 def divisao (numero1,numero2):     #Função para a divisão de dois números.
     return numero1/numero2
 
-op_usuario = int(input("------MENU-----\n"
-                         "1 - Adição\n"
-                         "2 - Subtração\n"
-                         "3 - Multiplicação\n"
-                         "4 - Divisão\n"
-                         "--------------------\n"
-                        "Digite o número da opção desejada: "))
+opcao_menu = {
+    1: "-----MENU----- \n1 - Adição",
+    2: "1 - Subtração",
+    3: "1 - Multiplicação",
+    4: "1 - Divisão",
+}
+
+
+for chave, valor in opcao_menu.items():
+    print(valor)
+
+
+opcao_usuario = int (input("\nDigite o número da opção desejada:"))
+
+
+
 
 
